f16974e9f251fd8aade768b82cccf4c8596ee002
Daniela <just@addsalt.ca>
2014-12-04 15:06:02 -0500

    Added navigation functionality

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-var $win = $(window);
-var $aboutWork = $('.about-work');
-
-$win.on('scroll', function(){
-    var scrollPos = $win.scrollTop();
-    if(scrollPos < 450) {
-        $aboutWork.css('margin-top',scrollPos/5)
+$(".nav-i>li").on('click',function(){
+    var $secondLevel=$(this).children('.second-level');
+    var isOpen=$secondLevel.attr('data-state');
+    
+    $('[data-state="visible"]').attr('data-state','closed');
+    $('[data-state="active"]').attr('data-state','inactive');
+    
+    if(isOpen !== 'visible'){
+        $secondLevel.attr('data-state','visible');
+        $(this).attr('data-state', 'active');
     }
-});
+})
